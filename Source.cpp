f76e176113a009f35aad990cffd9bb162f5eb330
bossmode344 <31901210+bossmode344@users.noreply.github.com>
2018-01-18 13:21:28 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,18 @@
-//write a program that prints a corny joke or pun to the console window :D
+#include <iostream>
+using namespace std;
+int answer;
+int main() {
+	cout << "How many HASHTAGS do you want?" << endl;
+	cin >> answer;
+	for (int i = 0; i < answer; i++) {
+		for (int k = 0; k < answer; k++)
+			cout << "#";
+		cout << endl << endl;
+	}
+
+
+
+
+
+
+}
